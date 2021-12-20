362539d25955ce2a11d5b639e30915b9b9cd2299
manassur <ologundemanasseh@gmail.com>
2021-12-20 03:40:05 +0100

    set up api method to hit endpoint

--- lib/bloc/courses/courses_bloc.dart
+++ lib/bloc/courses/courses_bloc.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:youth_action_handbook/bloc/courses/courses_event.dart';
 import 'package:youth_action_handbook/bloc/courses/courses_state.dart';
 import 'package:youth_action_handbook/models/course_response.dart';
+import 'package:youth_action_handbook/models/course_with_language_response.dart';
 import 'package:youth_action_handbook/services/api_service.dart';
 
 class CoursesBloc extends Bloc<CoursesEvent,CoursesState>{
@@ -18,8 +19,9 @@ Stream<CoursesState> mapEventToState(CoursesEvent event) async* {
   if (event is FetchCoursesEvent) {
     yield CoursesLoadingState();
     try{
-     CourseResponse courseResponse = await apiService.fetchCourses();
-      yield CoursesLoadedState(courses:courseResponse.courses, message: "Courses Updated");
+     CourseWithLanguageResponse courseWithLanguageResponse = await apiService.fetchCoursesFromServer();
+     print('hit course bloc');
+      yield CoursesLoadedState(courseWithLanguageResponse:courseWithLanguageResponse, message: "Courses Updated");
     }catch(e){
       yield CoursesLoadFailureState(error: e.toString());
     }
--- lib/bloc/courses/courses_state.dart
+++ lib/bloc/courses/courses_state.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:youth_action_handbook/models/course_response.dart';
+import 'package:youth_action_handbook/models/course_with_language_response.dart';
 
 abstract class CoursesState extends Equatable {
   @override
@@ -20,8 +21,9 @@ class CoursesLoadingState extends CoursesState {
 
 class CoursesLoadedState extends CoursesState {
  final List<Courses>? courses;
+ final CourseWithLanguageResponse? courseWithLanguageResponse;
  final  String? message;
-  CoursesLoadedState({@required this.courses, this.message});
+  CoursesLoadedState({@required this.courses, this.message,this.courseWithLanguageResponse});
 
   @override
   List<Object> get props => [];
